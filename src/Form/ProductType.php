6376ebb5f02bcff3c9a08f8055f875114b45aa73
Ikonik-Dev <as.par.stephane@gmail.com>
2022-04-19 19:24:33 +0200

    post ambulance

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,14 +2,8 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
 use App\Entity\Product;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,38 +12,13 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom du produit',
-                'attr' => [
-                    'placeholder' => 'Tapez le nom du produit'
-                ]
-            ])
-            ->add('shortDescription', TextareaType::class, [
-                'label' => 'Description courte',
-                'attr' => [
-                    'placeholder' => 'Tapez une description du produit'
-                ]
-            ])
-            ->add('price', MoneyType::class, [
-                'label' => 'Prix du produit',
-                'attr' => [
-                    'placeholder' => 'Tapez le prix du produit en €'
-                ]
-            ])
-
-            ->add('mainPicture', UrlType::class, [
-                'label' => 'Image du produit',
-                'attr' => ['placeholder' => 'Tapez une URL d\'image !']
-            ])
-
-            ->add('category', EntityType::class, [
-                'label' => 'categorie',
-                'placeholder' => '-- Choisir une Categorie--',
-                'class' => Category::class,
-                'choice_label' => function (Category $category) {
-                    return strtoupper($category->getName());
-                }
-            ]);   
+            ->add('name')
+            ->add('price')
+            ->add('slug')
+            ->add('mainPicture')
+            ->add('shortDescription')
+            ->add('category')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
